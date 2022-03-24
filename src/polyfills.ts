4ec1be0e490bf4324112cd4e706ef1c74867a528
li-rongcheng <Li.Rongcheng@gmail.com>
2022-03-25 02:29:54 +1100

    add auth ui

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -59,8 +59,11 @@ import 'zone.js';  // Included with Angular CLI.
  */
  import 'core-js/es/typed-array';
  import 'core-js/es/object';
+ import 'zone.js/dist/zone'; // Included with Angular CLI.
 
- (window as any).global = window;
- (window as any).process = {
-   env: { DEBUG: undefined },
- };
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
+
+(window as any).Zone['__zone_symbol__ignoreConsoleErrorUncaughtError'] = true;
